4eee7df76c11ee6d0b6e8bbad18173478637277a
R00tDroid <info@rootdroid.nl>
2024-09-09 20:30:21 +0200

    Define packer class

--- Source/AssetPacker.py
+++ Source/AssetPacker.py
@@ -1,1 +1,33 @@
-#TODO Setup packer logic
+import glob
+
+def AssetPacker():
+    def __init__(self):
+        self.output = ""
+        self.filters = []
+        self.files = []
+        self.hash = 0
+
+    def setOutput(self, output):
+        self.output = output
+
+    def add(self, filter):
+        self.filters += [filter]
+
+    def scan(self):
+        for filter in self.filters:
+            files = glob.glob(filter, recursive=True)
+        #TODO Find files
+        #TODO Calculate hash
+
+    def getFiles(self):
+        return self.files
+    
+    def needsUpdate(self):
+        return True
+         #TODO Check hash against cache
+    
+    def generate(self):
+        if not needsUpdate():
+            return
+        #TODO Generate output
+        #TODO Generate cache
